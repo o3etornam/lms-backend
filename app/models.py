e1ad5c572ae0994dd2937f74007f32150be24d01
o3etornam <03etornam@gmail.com>
2024-06-06 23:06:37 +0000

    added 2 more tables

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from sqlalchemy import Boolean, String, DateTime, Text, ForeignKey
+from sqlalchemy import Boolean, String, DateTime, Text, ForeignKey, Integer
 from sqlalchemy.orm import relationship, Mapped, mapped_column, DeclarativeBase
 from sqlalchemy.sql.expression import text
 
@@ -11,9 +11,23 @@ class Base(DeclarativeBase):
 class User(Base):
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(String(8), primary_key=True, nullable=False)
+    id: Mapped[str] = mapped_column(String(8), primary_key=True, nullable=False)
     email: Mapped[str] = mapped_column(String, nullable=False,unique=True)
     password: Mapped[str] = mapped_column(String, nullable= False)
     role: Mapped[str] = mapped_column(String, nullable=False)
     created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False, default=datetime.now)
     updated_at: Mapped[datetime] = mapped_column(DateTime, nullable=True)
+
+class Course(Base):
+    __tablename__ = "courses"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name: Mapped[str] = mapped_column(String, nullable=False,unique=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False, default=datetime.now)
+    updated_at: Mapped[datetime] = mapped_column(DateTime, nullable=True)
+
+class UserAndCourse(Base):
+    __tablename__ = 'users_and_courses'
+
+    course_id: Mapped[int] = mapped_column(Integer,  ForeignKey(column="courses.id", ondelete="cascade"), primary_key=True,nullable=False)
+    user_id: Mapped[str] = mapped_column(String, ForeignKey(column="users.id", ondelete="cascade"), primary_key=True, nullable=False)
