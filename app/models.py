8eb930a3cddefa4db0f07c68767333f9ba896e58
o3etornam <03etornam@gmail.com>
2024-06-06 23:07:56 +0000

    enrollment table

--- app/models.py
+++ app/models.py
@@ -26,8 +26,8 @@ class Course(Base):
     created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False, default=datetime.now)
     updated_at: Mapped[datetime] = mapped_column(DateTime, nullable=True)
 
-class UserAndCourse(Base):
-    __tablename__ = 'users_and_courses'
+class Enrollment(Base):
+    __tablename__ = 'enrollments'
 
     course_id: Mapped[int] = mapped_column(Integer,  ForeignKey(column="courses.id", ondelete="cascade"), primary_key=True,nullable=False)
     user_id: Mapped[str] = mapped_column(String, ForeignKey(column="users.id", ondelete="cascade"), primary_key=True, nullable=False)
